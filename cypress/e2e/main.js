8079e8cf0624fe83e7ade36e6fc6b38e94367380
leoalves226x <leoalves226x@gmail.com>
2024-03-31 22:35:36 -0300

    .

--- cypress/e2e/main.js
+++ cypress/e2e/main.js
@@ -4,6 +4,7 @@ describe ('Teste API', () => {
 
 
     beforeEach('Login', () => {
+        cy.intercept({method: 'Get', path: 'tags'}, {fixture: 'tags.json'})
         IrPara.TelaLogin()
     })
 
@@ -29,5 +30,26 @@ describe ('Teste API', () => {
     cy.contains('Teste Titulo').click()
     cy.contains('Delete Article').click()
 
+    })
+
+    it('Validar feed global', () => {
+
+      cy.intercept('GET','https://conduit-api.bondaracademy.com/api/articles/feed*' , {"articles":[],"articlesCount":0}).as('YourFeed')
+      cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', {fixture: 'articles.json'})
+  
+      cy.contains('Global Feed').click()
+      cy.get('app-article-list app-article-preview button').then(buttonList => {
+          expect(buttonList[0]).to.contain('1')
+          expect(buttonList[1]).to.contain('5')
+      })
+  
+      cy.fixture('articles.json').then(file =>{
+        const articleLink = file.articles[1].slug
+        file.articles[1].favoritesCount = 6
+        cy.intercept('POST','https://conduit-api.bondaracademy.com/api/articles/'+articleLink+'/favorite', file)
+      })
+  
+      cy.get('app-article-list app-article-preview button').eq(1).click().should('contain', '6')
+  
     })
 })
