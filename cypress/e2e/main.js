2cce213637986dbd51cba4f423f485b3d4191077
leoalves226x <leoalves226x@gmail.com>
2024-03-31 22:16:39 -0300

    .

--- cypress/e2e/main.js
+++ cypress/e2e/main.js
@@ -8,23 +8,26 @@ describe ('Teste API', () => {
     })
 
     it('Verificar request e response', () =>{
-
+    
     cy.intercept('POST','https://conduit-api.bondaracademy.com/api/articles/').as('postArticles')
     cy.contains('New Article').click()
-    cy.get('[formcontrolname="title"]').clear().type('This is a Test Title')
-    cy.get('[formcontrolname="description"]').clear().type('This is a Description')
-    cy.get('[formcontrolname="body"]').clear().type('This is the body text')
+    cy.get('[formcontrolname="title"]').clear().type('Teste Titulo')
+    cy.get('[formcontrolname="description"]').clear().type('Teste Descricao')
+    cy.get('[formcontrolname="body"]').clear().type('Teste body text')
     cy.contains('Publish Article').click()
 
     cy.wait('@postArticles').then(xhr => {
-      console.log(xhr)
-      if(xhr.response.statusCode == 200){
-        expect(xhr.response.statusCode).to.equal(200)
+      //console.log(xhr)
+      if(xhr.response.statusCode == 200 || xhr.response.statusCode == 201){
+        expect(xhr.request.body.article.description).to.equal('Teste Descricao')
+        expect(xhr.request.body.article.title).to.equal('Teste Titulo')
+        expect(xhr.request.body.article.body).to.equal('Teste body text')
       }
-      else if(xhr.response.statusCode == 201) {
-        expect(xhr.response.statusCode).to.equal(201)
-      }    
-      expect(xhr.request.body.article.description).to.equal('This is a Description')
     })
+
+    cy.get('li.nav-item').eq(3).click()
+    cy.contains('Teste Titulo').click()
+    cy.contains('Delete Article').click()
+
     })
 })
